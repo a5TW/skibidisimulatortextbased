ea5578107bf8b8b84684f8567e5f711cd74fca8d
a5TW <67015012+a5TW@users.noreply.github.com>
2024-11-15 09:22:40 -0500

    update

--- questionlist.py
+++ questionlist.py
@@ -1,1 +1,19 @@
+dialogue_a = ('''you have been knocked out and are in a empty room... what do you do?
+a. cry
+b. open the door
+c. look for the lights''')
 
+  dialogue_a_a = ('''you cry and lose 1 energy... what do you do now?
+  a. open the door
+  b. look for the lights
+  c. cry even more''')
+
+  dialogue_a_b = ('''you open the rigid door and lose 15 energy. you see a devestated place, unlike you ever imagined... you have 3 options, what do you take off the ground?
+  a. plunger
+  b. pepsi can
+  c. go back in room''')
+
+  dialogue_a_c = ('''you look for the lights and trip. you trip on some gooey thing, hitting your head on the lights. you lose 10 hp and 10 energy... what do you do?
+  a. you examine the gooey thing
+  b. you go open the back door
+  c. you examine the crack in the wall''')
